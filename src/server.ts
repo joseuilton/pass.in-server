9925cb558a7b1b90cf764c9adcf139b68f69fac8
joseuilton <joseuilton.siqueira@gmai.com>
2024-04-04 19:57:53 -0300

    feat: add get event requirement

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 const app = fastify();
 app.setSerializerCompiler(serializerCompiler);
@@ -9,6 +10,7 @@ app.setValidatorCompiler(validatorCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
 
 app.listen({ port: 3333 })
   .then(() => {
